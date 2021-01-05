5121b63db5330dda672ea3d09feb64a604eaba8f
ahasnawi <ahasnawi@madaincorp.com>
2021-01-05 23:03:16 +0200

    initial commit

--- script.js
+++ script.js
@@ -6,4 +6,4 @@ function chooseSpot(ele, num) {
    // Write your code here
 }
 
-// Wrire any additional finctions you need and try to be DRY 
+// Write any additional functions you need and try to be DRY 
