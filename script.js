ba14684df54dc839983ad11958cfb7cf56e6297e
ahasnawi <ahasnawi@madaincorp.com>
2021-01-05 22:58:43 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let arr = ['', '', '', '', '', '', '', '', '']
+let X = 'X';
+let O = 'O';
+
+function chooseSpot(ele, num) {
+   // Write your code here
+}
+
+// Wrire any additional finctions you need and try to be DRY 
